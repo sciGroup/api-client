5a03cfaa0e7ae76912dd9be4e19692d4f224500f
Artem Ostretsov <OstretsovAA@gmail.com>
2017-09-21 10:39:20 +0300

    ~ event search request

--- src/Model/Request/Event/EventSearchRequest.php
+++ src/Model/Request/Event/EventSearchRequest.php
@@ -34,8 +34,16 @@ class EventSearchRequest implements RequestInterface
     /** @var int */
     public $limit;
 
-    public function getQuery(): string
+    public function getQuery(string $relativeTo): string
     {
+        if (!$this->locale) {
+            throw new \LogicException(sprintf('Locale must be set before %s method call!', __METHOD__));
+        }
 
+        $data['text'] = $this->query ?: null;
+        $data['startDate'] = $this->startDate instanceof \DateTime ? $this->startDate->format('Y-m-d') : null;
+        $data['endDate'] = $this->endDate instanceof \DateTime ? $this->endDate->format('Y-m-d') : null;
+
+        return $relativeTo.sprintf('/open-api/v1/%s/event/events', $this->locale).http_build_query($data);
     }
 }
